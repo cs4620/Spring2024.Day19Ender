5b97e8853ad351dcfc9ef49c830d69c6ed179711
B. Ricks <bricks@unomaha.edu>
2024-04-09 12:10:41 -0500

    Use 3D points

--- fragmentShader.js
+++ fragmentShader.js
@@ -1,5 +1,6 @@
 fsSource = `
+    varying lowp vec4 vertexPos;
     void main() {
-      gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+      gl_FragColor = vec4(vertexPos.x, vertexPos.y, 1.0, 1.0);
     }
   `;
--- vertexShader.js
+++ vertexShader.js
@@ -1,8 +1,10 @@
 vsSource = `
+    varying vec4 vertexPos;
     attribute vec4 aVertexPosition;
     uniform mat4 uModelViewMatrix;
     uniform mat4 uProjectionMatrix;
     void main() {
+      vertexPos = aVertexPosition;
       gl_Position = mat4(1,0,0,0, 0,1,0,0, 0,0,1,0, 0,0,0,1) * uProjectionMatrix * uModelViewMatrix * aVertexPosition;
     }
 `;
