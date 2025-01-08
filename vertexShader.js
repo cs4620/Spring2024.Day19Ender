4d9cf3bc422c330f3b209a0e1b76e4f12f39d29f
bricks <bricks@unomaha.edu>
2025-01-08 15:43:03 -0600

    Adding a rotation component to the code

--- vertexShader.js
+++ vertexShader.js
@@ -5,6 +5,7 @@ vsSource = `
     uniform mat4 uProjectionMatrix;
     void main() {
       vertexPos = aVertexPosition;
-      gl_Position = mat4(1,0,0,0, 0,1,0,0, 0,0,(-10-(-1))/(-1-(-10)),-1, 0,0,(2*-10-(-1))/(-1-(-10)),0) /* * uProjectionMatrix */ * uModelViewMatrix * aVertexPosition;
+      //gl_Position = mat4(1,0,0,0, 0,1,0,0, 0,0,(-10-(-1))/(-1-(-10)),-1, 0,0,(2*-10-(-1))/(-1-(-10)),0) /* * uProjectionMatrix */ * uModelViewMatrix * aVertexPosition;
+      gl_Position =  uProjectionMatrix * uModelViewMatrix * aVertexPosition;
     }
 `;
